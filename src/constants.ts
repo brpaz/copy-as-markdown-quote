0d802deec982d8043cdccbd004bfec6bc766c58f
Bruno Paz <184563+brpaz@users.noreply.github.com>
2024-04-19 22:15:39 +0100

    feat: process html text (#7)
    * feat: process html text
    
    * lint fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,7 @@
 export const CONTEXT_MENU_ITEM_ID = 'copyAsMarkdownQuote'
-export const COPY_SELECTION_ACTION = 'copyAsMarkdownQuote:copySelection'
+export const CONTEXT_MENU_COPY_AS_HTML_ITEM_ID = 'copyAsMarkdownQuote-html'
+export const CONTEXT_MENU_COPY_AS_TEXT_ITEM_ID = 'copyAsMarkdownQuote-text'
+export const COPY_SELECTION_TEXT_ACTION =
+  'copyAsMarkdownQuote:copySelectionText'
+export const COPY_SELECTION_HTML_ACTION =
+  'copyAsMarkdownQuote:copySelectionHtml'
--- src/quote.ts
+++ src/quote.ts
@@ -1,4 +1,4 @@
-export function formatQuote(text: string, sourceURL: string): string {
+export function markdownQuoteFromText(text: string, sourceURL: string): string {
   // Split the selected text into lines
   const lines = text.split('\n')
 
@@ -9,4 +9,49 @@ export function formatQuote(text: string, sourceURL: string): string {
   const markdownQuote = `${quotedLines} [${sourceURL}](${sourceURL})`
 
   return markdownQuote
+}
+
+function replaceTag(str: string, tag: string, replacement: string): string {
+  return str
+    .replaceAll(`<${tag}> `, ` ${replacement}`)
+    .replaceAll(`<${tag}>`, replacement)
+    .replaceAll(` </${tag}>`, `${replacement} `)
+    .replaceAll(`</${tag}>`, replacement)
+}
+
+// Convert HTML to Markdown quote
+// Inspired by https://github.com/miniBill/copy-as-markdown/blob/main/background-script.js
+export function markdownQuoteFromHTML(html: string, sourceURL: string): string {
+  // replace relative links with absolute links
+  html = html.replace(/href="([^"]*)"/g, (_: string, href: string) => {
+    if (href.startsWith('http')) {
+      return `href="${href}"`
+    }
+    return `href="${new URL(href, sourceURL).href}"`
+  })
+
+  html = html
+    .replaceAll('</p>\n<p>', '\n> \n> ')
+    .replaceAll(
+      /<a(?: title="[^"]*")? href="([^"]*)"(?: [a-z]*="[^"]*")*>([^<]*)<\/a>/g,
+      (_: string, href: string, label: string) => `[${label}](${href})`,
+    )
+    .replaceAll(/ class="[^"]*"/g, '')
+    .replaceAll('&nbsp;', ' ')
+    .replaceAll('<br>', '\n> ')
+    .replaceAll(/^-/g, () => '\\-')
+
+  html = replaceTag(html, 'p', '')
+  html = replaceTag(html, 'sup', '')
+  html = replaceTag(html, 'em', '_')
+  html = replaceTag(html, 's', '~~')
+  html = replaceTag(html, 'i', '_')
+  html = replaceTag(html, 'strong', '**')
+  html = replaceTag(html, 'b', '**')
+  html = replaceTag(html, 'span', '')
+
+  // Convert everything to a markdown quote
+  html = html.replace(/^/gm, '> ')
+
+  return `${html} - [${sourceURL}](${sourceURL})`
 }
